cc67bab29246f6f035f84b539d0c529c9b77c3af
xuan thuy <ngxuanthuy1302@gmail.com>
2023-03-21 11:30:46 +0700

    ytb

---
+++ app.js
@@ -0,0 +1,4 @@
+// const mainContent = document.querySelector('.main_content_item')
+// let getwidthContent = mainContent.offsetWidth;
+// const line = document.querySelector('.line')
+// line.style.width = (getwidthContent * 3 + 16 * 2) + 'px';
